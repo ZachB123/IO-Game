200324fdb143216d09e3308b806bddb643f78eb8
Zach <zachary-buchholz@uiowa.edu>
2022-03-01 11:15:46 -0600

    script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 "use strict";
-console.log('Hello World');
-console.log("yeet");
+let canvas;
+{
+    let tempCanvas = document.querySelector("#game");
+    if (tempCanvas !== null) {
+        canvas = tempCanvas;
+        init();
+    }
+    else {
+        alert("Need to enable JavaScript or something else has gone wrong");
+    }
+}
+let screenWidth;
+let screenHeight;
+function init() {
+    handleResize();
+}
+function handleResize() {
+    screenWidth = document.documentElement.clientWidth;
+    screenHeight = document.documentElement.clientHeight;
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+addEventListener("resize", handleResize);
--- ts/script.ts
+++ ts/script.ts
@@ -1,2 +1,28 @@
-console.log('Hello World');
-console.log("yeet");
+
+let canvas: HTMLCanvasElement;
+
+{
+    let tempCanvas: HTMLCanvasElement | null = document.querySelector("#game");
+    if(tempCanvas !== null) {
+        canvas = tempCanvas;
+        init();
+    } else {
+        alert("Need to enable JavaScript or something else has gone wrong");
+    }
+}
+
+let screenWidth;
+let screenHeight;
+
+function init(): void {
+    handleResize();
+}
+
+function handleResize(): void {
+    screenWidth = document.documentElement.clientWidth;
+    screenHeight = document.documentElement.clientHeight;
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+addEventListener("resize", handleResize);
